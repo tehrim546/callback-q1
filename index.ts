36276d94d9727dad67d4f66e282d1890abeb8e3e
= <=>
2024-01-30 10:07:01 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+//Implement a callback function within a setTimeout to log a message after a delay. Be aware of the asynchronous nature of setTimeout.
+function delayedLog(message: string,delay:number):void{
+    setTimeout(() =>{
+        console.log(message);
+    },delay);
+}
+// example usage:
+console.log("Start");
+delayedLog("Delayed message after 2000 milliseconds",2000),
+console.log("End");
